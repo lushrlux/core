9c49f81416ced964dca57e080e7cedb9bcf4f9c3
Tom Nicklin <tom.nicklin@boomin.com>
2020-09-21 22:19:57 +0100

    new first commit

---
+++ lib/lushrlux.ex
@@ -0,0 +1,18 @@
+defmodule Lushrlux do
+  @moduledoc """
+  Documentation for `Lushrlux`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Lushrlux.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/lushrlux_test.exs
@@ -0,0 +1,8 @@
+defmodule LushrluxTest do
+  use ExUnit.Case
+  doctest Lushrlux
+
+  test "greets the world" do
+    assert Lushrlux.hello() == :world
+  end
+end
